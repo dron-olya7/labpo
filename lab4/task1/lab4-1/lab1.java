af165fc761762bbd700a452cf86d0e8f9a94bb1a
Оля <71034985+dron-olya7@users.noreply.github.com>
2023-09-23 17:00:27 +0500

    Delete lab4 directory

--- lab4/task1/lab4-1/lab1.java
+++
@@ -1,7 +0,0 @@
-public class lab1 {
-    Number n;
-        if( flag )
-            n = Double.valueOf((double) ( new Integer(1).intValue() ));
-        else
-        n = Double.valueOf(new Double(2.0).doubleValue());
-}
--- lab4/task1/lab4-5/src/main/java/org/example/Main.java
+++
@@ -1,9 +0,0 @@
-package org.example;
-
-public class Main {
-    public static void main(String[] args) {
-
-        System.out.printf("%s\n", "str#1");
-        System.out.println("str#2");
-    }
-}
