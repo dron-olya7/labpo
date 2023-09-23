ec25dae7e0e95284ef59ca7209c4ebece05dfaae
Olga Dron <olya_sok00@mail.ru>
2023-09-23 15:19:08 +0500

    Лабораторная работа 4

---
+++ lab4/task1/lab4-1/lab1.java
@@ -0,0 +1,7 @@
+public class lab1 {
+    Number n;
+        if( flag )
+            n = Double.valueOf((double) ( new Integer(1).intValue() ));
+        else
+        n = Double.valueOf(new Double(2.0).doubleValue());
+}
